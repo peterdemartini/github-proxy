dbbffc02947a8a84493cb600d64ec426c87d94a6
Peter DeMartini <peterdemartini@me.com>
2014-05-23 09:11:07 -0700

    added cors

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/contributions/:username', function (req, res) {
     rest('https://github.com/users/' + req.params.username + '/contributions_calendar_data')
-    .then(function(response) {
-        var results = response.entity || [];
-        res.json(JSON.parse(results));
-    });
+        .then(function (response) {
+            var results = response.entity || [];
+            res.json(JSON.parse(results));
+        });
 });
 
 var port = Number(process.env.PORT || 5000);
